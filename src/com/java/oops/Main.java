aca98366643ed61082384ac25d7c6883c84ebe06
Adithya-vajrala <vajrlaadithya@gmail.com>
2026-04-15 15:39:20 +0530

    chore: Initial commit

---
+++ src/com/java/oops/Main.java
@@ -0,0 +1,8 @@
+package com.java.oops;
+public class Main {
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.id = 522141;
+
+    }
+}
---
+++ src/com/java/oops/Product.java
@@ -0,0 +1,15 @@
+package com.java.oops;
+
+public class Product {
+    int id;
+    String category;
+    String name;
+    String company;
+    int maxRetailPrice;
+    byte discountPercentage;
+    double discountAmount ;
+    int finalSellingPrice ;
+    float rating;
+    short ratingCount;
+    boolean isAvailable;
+    }

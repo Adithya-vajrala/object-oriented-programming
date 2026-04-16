a09c61a404025459b6b8c503fe93d314ff5a1cfe
Adithya-vajrala <vajrlaadithya@gmail.com>
2026-04-16 13:35:05 +0530

    feat:Customer is added

---
+++ src/com/java/oops/Customer.java
@@ -0,0 +1,12 @@
+package com.java.oops;
+
+public class Customer {
+    int id;
+    String name;
+    String emailId;
+    long phoneNo;
+    char gender;
+    String password;
+    int pincode;
+    String address;
+}

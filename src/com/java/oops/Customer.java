aab9b9a9af65f7b096e9bc0d3f7211314ae9733c
Adithya-vajrala <vajrlaadithya@gmail.com>
2026-04-17 15:32:23 +0530

    refactor: improve Customer and Product

--- src/com/java/oops/Customer.java
+++ src/com/java/oops/Customer.java
@@ -9,4 +9,24 @@ public class Customer {
     String password;
     int pincode;
     String address;
+    Customer(){
+        id = 20;
+        name = "aditya";
+        emailId = "adithya@gamil.com";
+        phoneNo = 99392723505L;
+        gender = 'M';
+        password = "12323";
+        pincode = 522141;
+        address= "vinukonda";
+    }
+    public Customer(String name, int id, String emailId, long phoneNo, char gender, String password, int pincode, String address) {
+        this.name = name;
+        this.id = id;
+        this.emailId = emailId;
+        this.phoneNo = phoneNo;
+        this.gender = gender;
+        this.password = password;
+        this.pincode = pincode;
+        this.address = address;
+    }
 }
--- src/com/java/oops/Product.java
+++ src/com/java/oops/Product.java
@@ -8,9 +8,39 @@ public class Product {
     String description;
     int maxRetailPrice;
     byte discountPercentage;
-    int finalSellingPrice ;
+    int finalSellingPrice;
     float rating;
     short ratingCount;
     boolean isAvailable;
     byte size;
+
+    public Product() {
+        id = 1324;
+        category = "slippers";
+        name = "slipper";
+        company = "paragon";
+        description = "Very comfortable";
+        maxRetailPrice = 134;
+        discountPercentage = 10;
+        finalSellingPrice = 100;
+        rating = 4.6F;
+        ratingCount = 100;
+        isAvailable = true;
+        size = 9;
     }
+    public Product(String category, int id, String name, String company, String description, int maxRetailPrice, byte discountPercentage, int finalSellingPrice, float rating, short ratingCount, boolean isAvailable, byte size) {
+        this.category = category;
+        this.id = id;
+        this.name = name;
+        this.company = company;
+        this.description = description;
+        this.maxRetailPrice = maxRetailPrice;
+        this.discountPercentage = discountPercentage;
+        this.finalSellingPrice = finalSellingPrice;
+        this.rating = rating;
+        this.ratingCount = ratingCount;
+        this.isAvailable = isAvailable;
+        this.size = size;
+    }
+}
+

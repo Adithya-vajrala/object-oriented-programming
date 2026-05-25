18902085b47f0871498616bf3ecb081f7bd63f30
Adithya-Vajrala <vajrlaadithya@gmail.com>
2026-05-25 15:51:55 +0530

    refactor: collections added.

---
+++ src/com/java/collectionassignment/Main.java
@@ -0,0 +1,8 @@
+package com.java.collectionassignment;
+
+public class Main {
+    public static void main(String[] args) {
+        CsvReader reader = new CsvReader();
+        reader.getProductsFromCsv();
+    }
+}
